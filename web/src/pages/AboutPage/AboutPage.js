f6cb49a350573b3bb01b06db71898cdedaa1c70d
ncucciniello <ncucciniello@gmail.com>
2020-09-29 16:43:58 -0400

    create/link basic home and about pages

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>
+        This site was created to demo my mastery of Redwood: Look on my works, ye mighty, and despair!  
+      </p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
