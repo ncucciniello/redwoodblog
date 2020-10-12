4a0efa92a21d92ff162e5d299eb0009974063225
ncucciniello <ncucciniello@gmail.com>
2020-10-12 12:53:30 -0400

    implement netlify identify

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,25 +1,32 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
+
   return (
-    <>
-      <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.contact()}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <div>
+      <h1>
+        <Link to={routes.home()}>Redwood Blog</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
+          </li>
+          <li>
+            <Link to={routes.contact()}>Contact</Link>
+          </li>
+          <li>
+            <a href="#" onClick={isAuthenticated ? logOut : logIn}>
+              {isAuthenticated ? 'Log Out' : 'Log In'}
+            </a>
+          </li>
+          {isAuthenticated && <li>{currentUser.email}</li>}
+        </ul>
+      </nav>
       <main>{children}</main>
-    </>
+    </div>
   )
 }
 
